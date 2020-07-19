687ce358de38b00f67bc632c74ab91bf4d98bea1
Arekor <bunner.benjamin@gmail.com>
2020-07-20 00:01:42 +0200

    Feature (MonsterList) : Adapter

---
+++ app/src/main/java/com/arekor/gm_helper/monster/MonsterAdapter.kt
@@ -0,0 +1,40 @@
+package com.arekor.gm_helper.monster
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.arekor.gm_helper.R
+import com.arekor.gm_helper.data.model.Monster
+import kotlinx.android.synthetic.main.monster_list_item.view.*
+
+class MonsterAdapter(val monsters: List<Monster>) : RecyclerView.Adapter<MonsterAdapter.MonsterViewHolder>() {
+
+    class MonsterViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val title = itemView.monster_list_item_title
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)
+            : MonsterAdapter.MonsterViewHolder {
+        val v: View = LayoutInflater.from(parent.context)
+            .inflate(R.layout.monster_list_item,parent,false)
+        return MonsterViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: MonsterAdapter.MonsterViewHolder, position: Int) {
+        holder.title.text = monsters[position].name
+    }
+
+    override fun getItemCount(): Int {
+        return monsters.size
+    }
+
+    override fun getItemId(position: Int): Long {
+        return super.getItemId(position)
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return super.getItemViewType(position)
+    }
+
+}
