d1b35190c87eeeff797859893c0884caa29f9d0f
Arekor <bunner.benjamin@gmail.com>
2020-08-05 23:04:58 +0200

    Feature (Window) : Transparent theme

--- app/src/main/java/com/arekor/gm_helper/MainActivity.kt
+++ app/src/main/java/com/arekor/gm_helper/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.arekor.gm_helper
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.arekor.gm_helper.creature.CreatureViewModel
 
@@ -10,6 +11,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, WindowManager.LayoutParams.TYPE_STATUS_BAR)
         setContentView(R.layout.activity_main)
     }
 }
