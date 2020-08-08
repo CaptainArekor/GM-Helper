162fb87b10a4f994a9c378792d808d6dcf377f3d
Arekor <bunner.benjamin@gmail.com>
2020-08-08 17:12:02 +0200

    Feature (Menu) : Changing display + New default font

--- app/src/main/java/com/arekor/gm_helper/navigation/NavigationMenuFragment.kt
+++ app/src/main/java/com/arekor/gm_helper/navigation/NavigationMenuFragment.kt
@@ -25,41 +25,26 @@ class NavigationMenuFragment : Fragment() {
         view.menu_creatures.setOnClickListener {
             openMonsterList()
         }
-        view.menu_02.setOnClickListener {
-            println("Menu 02")
+        view.menu_group.setOnClickListener {
+            openGroupList()
         }
-        view.menu_03.setOnClickListener {
-            println("Menu 03")
+        view.menu_item.setOnClickListener {
+            openItemList()
         }
-        /*Picasso.get()
-            .load(R.drawable.background_image_forest)
-            .transform(BlurTransformation(context, 50, 1))
-            .centerCrop()
-            .fit()
-            .into(view.navigation_menu_background)*/
         return view
-    }
-
-    override fun onStart() {
-        super.onStart()
-        //setNavigationBar()
-    }
-
-    private fun setNavigationBar() {
-        val navigationBar = childFragmentManager.fragments.first() as NavigationBarFragment
-        navigationBar.setTitle(getString(R.string.app_name))
-        navigationBar.showBackButton(false)
-        navigationBar.showValidateButton(false)
-    }
-
-    private fun toastMe(text: String) {
-        Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
     }
 
     private fun openMonsterList() {
         findNavController().navigate(R.id.monsterListFragment)
     }
-
+    private fun openGroupList() {
+        //findNavController().navigate(R.id.monsterListFragment)
+        Toast.makeText(context,"Group",Toast.LENGTH_SHORT).show()
+    }
+    private fun openItemList() {
+        //findNavController().navigate(R.id.monsterListFragment)
+        Toast.makeText(context,"Item",Toast.LENGTH_SHORT).show()
+    }
     private fun openDice() {
         findNavController().navigate(R.id.diceFragment)
     }
